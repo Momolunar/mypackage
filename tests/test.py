d7f5a1a06a4d1feeeb0f44e2cf6793424e9abd4c
Monde Colephi <mondelunar@gmail.com>
2019-03-19 15:51:09 +0200

    updating setup

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from recsort import myFunction
+from recsort import recursion, sorting
 
 def test_top_n():
     """
@@ -14,50 +14,50 @@ def test_sum_array():
     make sure sum_array function works correctly
     '''
 
-    assert myFunction.sum_array([1,2,3,4,5]) == 15, 'incorrect'
-    assert myFunction.sum_array([10,2,1]) == 13, 'incorrect'
-    assert myFunction.sum_array([2]) == 2, 'incorrect'
+    assert recursion.sum_array([1,2,3,4,5]) == 15, 'incorrect'
+    assert recursion.sum_array([10,2,1]) == 13, 'incorrect'
+    assert recursion.sum_array([2]) == 2, 'incorrect'
 
 def test_fibonnaci():
     '''
     make sure fibonacci function works correctly
     '''
 
-    assert myFunction.fibonacci(14) == 377, 'incorrect'
-    assert myFunction.fibonacci(0) == 0, 'incorrect'
-    assert myFunction.fibonacci(7) == 13, 'incorrect'
+    assert recursion.fibonacci(14) == 377, 'incorrect'
+    assert recursion.fibonacci(0) == 0, 'incorrect'
+    assert recursion.fibonacci(7) == 13, 'incorrect'
 
 def test_factorial():
     '''
     make sure factorial function works correctly
     '''
 
-    assert myFunction.factorial(5) == 120, 'incorrect'
-    assert myFunction.factorial(10) == 3628800, 'incorrect'
-    assert myFunction.factorial(3) == 6, 'incorrect'
+    assert recursion.factorial(5) == 120, 'incorrect'
+    assert recursion.factorial(10) == 3628800, 'incorrect'
+    assert recursion.factorial(3) == 6, 'incorrect'
 
 def test_bubble_sort():
     '''
     make sure bubble_sort function works correctly
     '''
 
-    assert myFunction.bubble_sort([10, 100, 5, 12, 55]) == [5, 10, 12, 55, 100], 'incorrect'
-    assert myFunction.bubble_sort(['aa', 'xx', 'zz', 'bb', 'cc']) == ['aa', 'bb', 'cc', 'xx', 'zz'], 'incorrect'
-    assert myFunction.bubble_sort(['patch', 'tour', 'yak', 'zombie', 'egg', 'stall']) == ['egg', 'patch', 'stall', 'tour', 'yak', 'zombie'], 'incorrect'
+    assert sorting.bubble_sort([10, 100, 5, 12, 55]) == [5, 10, 12, 55, 100], 'incorrect'
+    assert sorting.bubble_sort(['aa', 'xx', 'zz', 'bb', 'cc']) == ['aa', 'bb', 'cc', 'xx', 'zz'], 'incorrect'
+    assert sorting.bubble_sort(['patch', 'tour', 'yak', 'zombie', 'egg', 'stall']) == ['egg', 'patch', 'stall', 'tour', 'yak', 'zombie'], 'incorrect'
 
 
 def test_reverse():
     '''
     make sure reverse function works correctly
     '''
-    assert myFunction.reverse('hello') == 'olleh', 'incorrect'
-    assert myFunction.reverse('hello there') == 'ereht olleh', 'incorrect'
-    assert myFunction.reverse('A') == 'A', 'incorrect'
+    assert sorting.reverse('hello') == 'olleh', 'incorrect'
+    assert sorting.reverse('hello there') == 'ereht olleh', 'incorrect'
+    assert sorting.reverse('A') == 'A', 'incorrect'
 
 def test_merge_sort():
     '''
     make sure merge_sort function works correctly
     '''
-    assert myFunction.merge_sort([10, 100, 5, 12, 55]) == [5, 10, 12, 55, 100], 'incorrect'
-    assert myFunction.merge_sort(['aa', 'xx', 'zz', 'bb', 'cc']) == ['aa', 'bb', 'cc', 'xx', 'zz'], 'incorrect'
-    assert myFunction.merge_sort(['patch', 'tour', 'yak', 'zombie', 'egg', 'stall']) == ['egg', 'patch', 'stall', 'tour', 'yak', 'zombie'], 'incorrect'
+    assert sorting.merge_sort([10, 100, 5, 12, 55]) == [5, 10, 12, 55, 100], 'incorrect'
+    assert sorting.merge_sort(['aa', 'xx', 'zz', 'bb', 'cc']) == ['aa', 'bb', 'cc', 'xx', 'zz'], 'incorrect'
+    assert sorting.merge_sort(['patch', 'tour', 'yak', 'zombie', 'egg', 'stall']) == ['egg', 'patch', 'stall', 'tour', 'yak', 'zombie'], 'incorrect'
